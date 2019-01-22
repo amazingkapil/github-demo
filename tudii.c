f803db92f49b2e5e8ba3aa8b4ef36522332ee9c3
Akash Kapil <ash1990su@gmail.com>
2019-01-22 16:16:07 +0530

    ye loo

--- tudii.c
+++ tudii.c
@@ -2,50 +2,44 @@
 using namespace std;
 int main()
 {
-int a;	
+int a , b;	
  string n , s ;
  cout << " enter your name: ";
  cin >> n;
  cin >> s;
 
-while (1){
+while (1);
+{
  cout << " chooes one optin \n" << " press 1 for voting the actor \n" << " press 2 for see the best actor \n" << " press 3 for exit \n";
  cout << "\n" << "enter your key: ";
  cin >> a;
   cout <<"\n\n"<< " 1. Varun Dhawan \n" << " 2. Shidarth Malhothra \n" << " 3. Ranbeer Shingh \n" << " 4. Sahid Kapoor \n";
 
 cout << "\n" << "Vote for Actors: ";
- cin >> a;
+ cin >> b;
 
-if (a==1){
+if (b == 1){
 	cout << " your vote goes to  Varun Dhawan \n";
 }    
-if (a==2){
+if (b == 2){
 	cout << " your vote goes to Shidarth Malhothra \n";
 }
-if (a==3){
+if (b == 3){
 	cout << " your vote goes to Ranbeer Shingh \n";
 }
-if (a==4){
+if (b == 4){
 	cout << " your vote goes to Sahid Kapoor \n";
 } 
 
+break;
+}
 
-cout<<"\n\n"  << " press 1 for voting the actor \n" << " press 2 for see the best actor \n" << " press 3 for exit \n";
- cout << "\n" << "enter your key: ";
- 
-
-cout << a << "  is the best actor \n";
- 
-  cout<<"\n\n"  << " press 1 for voting the actor \n" << " press 2 for see the best actor \n" << " press 3 for exit \n";
- cout << "\n" << "enter your key: ";
- cin >> a;
-  
   if (a==3){
   	cout << "exit\n";
+
   }
-   break;
-}
+   
+
 
    return 0;
 }
