805ced858b953c80a4ff0e8fce9f76ae76f04061
Akash Kapil <ash1990su@gmail.com>
2019-01-24 16:27:00 +0530

    lijiye

--- dollerstar.c
+++ dollerstar.c
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-
-int a , b , c;
+while(1){
+int a,b,c,d,e;
 
 cout << " chooes your option \n" << " 1. for put your  value\n " <<" 2. for see the result \n";
 cout << " enter your value: ";
@@ -19,7 +19,7 @@ cout << "give two values: " ;
 
    
 	cin >>a >> b;
-for (int i = 2 ; i <= b; ++i)
+for (int i = a ; i <= b; ++i)
 {
  
 
@@ -36,26 +36,26 @@ for (int i = 2 ; i <= b; ++i)
 }
 
 
-
 if (c == 2)
  {
+cout << "give  values: " ;
 
-	int a = 2,b = 8;
-		cin >> a >> b;
-for (int i = 2 ; i <= b; ++i)
-  { 
+	
+		cin >> a >> b >> d >> e;
+   for (int i = a; i = b ;++i)
+ 
 
  if (i%2==0){                     
 		cout <<"\n"<< " * ";
 	}
-	else 
+	else
 	 	cout << "\n" << " $ ";
 	
 	cout <<"\n";
     
-  }    
+    break;
 
  }
 
 }
-
+}
